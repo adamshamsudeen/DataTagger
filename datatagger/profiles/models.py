255cc3a928bb8249c2b6f93ace6df9f6d69d9059
Adam <adamshamsudeen@gmail.com>
2019-02-22 15:57:14 +0530

    added translation and profile creation

--- datatagger/profiles/models.py
+++ datatagger/profiles/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 # from translation.models import LanguageText
 User = settings.AUTH_USER_MODEL
 
@@ -10,18 +11,43 @@ class LanguageText(models.Model):
     def __str__(self):
         return self.language
 
+
 class Profile(models.Model):
     user              = models.OneToOneField(User, on_delete=models.CASCADE) # user.profile
     profession        = models.CharField(max_length=120, blank=True, null=True)
     activated         = models.BooleanField(default=True)
     timestamp         = models.DateTimeField(auto_now_add=True)
     updated           = models.DateTimeField(auto_now=True)
-    contributions     = models.IntegerField()
-    language          = models.ManyToManyField(LanguageText)
+    contributions     = models.IntegerField(default='0')
+    language          = models.ManyToManyField(LanguageText, blank=True)
 
 
     def __str__(self):
         return self.user.username
+        
+    def send_activation_email(self):
+        if True:#not self.activated:
+            # self.activation_key = code_generator()# 'somekey' #gen key
+            self.save()
+            #path_ = reverse()
+            # path_ = reverse('activate', kwargs={"code": self.activation_key})
+            # full_path = "https://startuplog.co/" + path_
+            # subject = 'Activate Account'
+            # from_email = settings.DEFAULT_FROM_EMAIL
+            # message = f'Activate your account here: {full_path}'
+            # recipient_list = [self.user.email]
+            # html_message = f'<p>Activate your account here: <a href="{full_path}">Activate Email</a></p>'
+            # print(html_message)
+            # sent_mail = send_mail(
+            #                 subject, 
+            #                 message, 
+            #                 from_email, 
+            #                 recipient_list, 
+            #                 fail_silently=False, 
+            #                 html_message=html_message)
+            # # print(sent_mail)
+            # sent_mail = False
+            return #sent_mail
 
 def default_user():
     return Profile.objects.get(pk=1).pk
@@ -33,3 +59,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.project_name
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        profile, is_created = Profile.objects.get_or_create(user=instance)
+        # default_user_profile = Profile.objects.get_or_create(user__id=1)[0] #user__username=
+        # default_user_profile.followers.add(instance)
+        #profile.followers.add(default_user_profile.user)
+        #profile.followers.add(2)
+
+post_save.connect(post_save_user_receiver, sender=User)
--- datatagger/translation/views.py
+++ datatagger/translation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import TranslateOrigin
+from .models import TranslateOrigin, TranslatedText
 from django import forms
+from profiles.models import LanguageText
 from .forms import TranslateForm
 # Create your views here.
 
@@ -15,6 +16,13 @@ def translate(request):
             text = form.cleaned_data['translated_text']
             language = form.cleaned_data['language']
             print(text, language)
+            language, _ = LanguageText.objects.get_or_create(language=language)
+            translation = TranslatedText(language=language,translated_text=text,
+                        tagged_by = request.user.profile,
+                        origin_text = tranlate_test,
+                        )
+            translation.save()
+
 
     else:
         form = TranslateForm()
