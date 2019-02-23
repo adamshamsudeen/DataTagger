2a13262d83e8c1a620e484aae63aba9de0d56c86
Adam <adamshamsudeen@gmail.com>
2019-02-23 14:32:46 +0530

    added user message

--- datatagger/translation/views.py
+++ datatagger/translation/views.py
@@ -8,7 +8,29 @@ from .forms import TranslateForm
 
 def translate(request):
     # View code here...
-    tranlate_test = TranslateOrigin.objects.get()
+    if request.user.is_authenticated:
+        chosen = True
+        user_languages = request.user.profile.language.values_list('language',flat=True)
+        if 'ml' in user_languages:
+            print("ml - user")
+            language = LanguageText.objects.get(language='ml')
+            
+        elif 'ta' in user_languages:
+            print("ta - user")
+            language = LanguageText.objects.get(language='ta')
+        else:
+            chosen = False
+
+        
+        
+    else:
+        chosen = False
+
+    if not chosen:
+        to_translate = TranslateOrigin.objects.last()
+    else:
+        to_translate = TranslateOrigin.objects.get(language=language)
+
     if request.method == 'POST':
         form = TranslateForm(request.POST)
         # print(request.json())
@@ -19,11 +41,12 @@ def translate(request):
             language, _ = LanguageText.objects.get_or_create(language=language)
             translation = TranslatedText(language=language,translated_text=text,
                         tagged_by = request.user.profile,
-                        origin_text = tranlate_test,
+                        origin_text = to_translate,
                         )
             translation.save()
 
 
     else:
         form = TranslateForm()
-    return render(request, 'translate.html', context={'origin': tranlate_test, 'form':form})
+    print(chosen)
+    return render(request, 'translate.html', context={'origin': to_translate, 'form':form,'profile_filled':chosen})
