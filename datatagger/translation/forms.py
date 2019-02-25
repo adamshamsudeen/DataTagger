4bb793304c3588631655d579432bc6b054bf3924
Adam <adamshamsudeen@gmail.com>
2019-02-25 16:12:02 +0530

    fixed bug with traslate source mismatch

--- datatagger/translation/forms.py
+++ datatagger/translation/forms.py
@@ -3,6 +3,7 @@ from django import forms
 class TranslateForm(forms.Form):
     translated_text = forms.CharField(max_length=250,widget=forms.Textarea(attrs={'placeholder': 'Enter the translation here.','cols': 80, 'rows': 10}))
     language = forms.CharField(max_length=100, initial='en')
+    origin = forms.CharField(max_length=100, widget=forms.HiddenInput())
     # message = forms.CharField(widget=forms.Textarea)
     # sender = forms.EmailField()
     # cc_myself = forms.BooleanField(required=False)
--- datatagger/translation/views.py
+++ datatagger/translation/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import  reverse_lazy
 from .models import TranslateOrigin, TranslatedText
 from django import forms
 from profiles.models import LanguageText
@@ -41,13 +43,18 @@ def translate(request):
         if form.is_valid():
             text = form.cleaned_data['translated_text']
             language = form.cleaned_data['language']
+            print(form.cleaned_data['origin'])
+            orig_pk = form.cleaned_data['origin']
             print(text, language)
             language, _ = LanguageText.objects.get_or_create(language=language)
             translation = TranslatedText(language=language,translated_text=text,
                         tagged_by = request.user.profile,
-                        origin_text = to_translate,
+                        origin_text = TranslateOrigin.objects.get(pk=orig_pk),
                         )
             translation.save()
+            form = TranslateForm()
+            # HttpResponseRedirect('/translate/')
+        # return reverse_lazy('translate')
 
 
     else:
