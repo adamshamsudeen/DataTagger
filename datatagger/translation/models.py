8a62d9b1db9cc2715281e49ee44dc3b01cdea959
Adam <adamshamsudeen@gmail.com>
2019-02-25 14:30:02 +0530

    bug fix : removed pos_save in translatedtext

--- datatagger/translation/models.py
+++ datatagger/translation/models.py
@@ -42,13 +42,3 @@ class TranslatedText(models.Model):
 
 
 
-def post_save_user_receiver(sender, instance, created, *args, **kwargs):
-    if created:
-        
-        profile, is_created = Profile.objects.get_or_create(user=instance)
-        # default_user_profile = Profile.objects.get_or_create(user__id=1)[0] #user__username=
-        # default_user_profile.followers.add(instance)
-        #profile.followers.add(default_user_profile.user)
-        #profile.followers.add(2)
-
-post_save.connect(post_save_user_receiver, sender=TranslatedText)
