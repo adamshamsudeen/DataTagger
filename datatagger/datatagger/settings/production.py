569b855f863311756ccf7ef9a4dd6ebc42542408
Adam <adamshamsudeen@gmail.com>
2019-03-23 18:44:00 +0530

    settings change for postgres

--- datatagger/datatagger/settings/production.py
+++ datatagger/datatagger/settings/production.py
@@ -81,7 +81,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tagger',
+        'USER': 'tagger_user',
+        'PASSWORD': 'tagger_pa$$',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
