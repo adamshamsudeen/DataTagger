cd41a35a762f709347858aa2c2aac3817cd2851c
Adam <adamshamsudeen@gmail.com>
2019-03-25 21:31:26 +0530

    final script

--- datatagger/script.py
+++ datatagger/script.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import User
 from translation.models import TranslateOrigin
 from profiles.models import Project, LanguageText
-import tqdm
+from tqdm import tqdm
 
 
 
-with open('./malaylam.txt') as f:
+with open('./malayalam.txt') as f:
+    project = Project.objects.get(pk=1)
+    language, _ = LanguageText.objects.get_or_create(language='ml')
     for line in tqdm(f):
-        project = Project.objects.get(pk=1)
-        language = LanguageText.objects.get_or_create(language='ml')
+        # print(line)
         _, created = TranslateOrigin.objects.get_or_create(
             project_name=project,
             language=language,
