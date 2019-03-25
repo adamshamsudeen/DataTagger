21c26da74de860fca52350b8468fc34b1edd598b
Adam <adamshamsudeen@gmail.com>
2019-03-25 19:16:08 +0530

    removed static_dir

--- datatagger/datatagger/settings/production.py
+++ datatagger/datatagger/settings/production.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
